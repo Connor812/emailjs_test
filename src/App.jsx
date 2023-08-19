b13770f4994cab6160e505ca640a45434db721f0
Connor812 <connorsavoy812@gmail.com>
2023-08-19 13:59:28 -0400

    first commit for test emailjs

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './App.css';
+
+export const App = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      import.meta.env.VITE_SERVICE_ID,
+      import.meta.env.VITE_TEMPLATE_ID,
+      form.current,
+      import.meta.env.VITE_PUBLIC_KEY
+    )
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+  return (
+    <form className="form" ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="from_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default App;
